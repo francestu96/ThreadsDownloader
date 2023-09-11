2a97ed0d2058c2af3c15cacc77327c066e831522
francestu96 <francestu96@gmail.com>
2023-09-11 18:22:32 +0200

    sitemap

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,53 @@
+const URL = "https://threadsdownloader.studio";
+
+function generateSiteMap(locales: string[]) {
+    // return `<?xml version="1.0" encoding="UTF-8"?></xml>`;
+    return `<?xml version="1.0" encoding="UTF-8"?>
+     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       <!--We manually set the two URLs we know already-->
+       <url>
+         <loc>${URL}</loc>
+       </url>
+       <url>
+         <loc>${URL}/faq</loc>
+       </url>
+       <url>
+         <loc>${URL}/privacy-policy</loc>
+       </url>
+       <url>
+         <loc>${URL}/terms-of-service</loc>
+       </url>
+       ${locales.map(x => {
+           return `
+            <url>
+                <loc>${`${URL}/${x}`}</loc>
+            </url>
+            <url>
+                <loc>${`${URL}/${x}/faq`}</loc>
+            </url>
+            <url>
+                <loc>${`${URL}/${x}/privacy-policy`}</loc>
+            </url>
+            <url>
+                <loc>${`${URL}/${x}/terms-of-service`}</loc>
+            </url>`;
+         }).join('')}
+     </urlset>
+   `;
+  }
+  
+  function SiteMap() { }
+  
+  export async function getServerSideProps({ res, locales }: any) {
+    const sitemap = generateSiteMap(locales);
+  
+    res.setHeader('Content-Type', 'text/xml');
+    res.write(sitemap);
+    res.end();
+  
+    return {
+      props: {},
+    };
+  }
+  
+  export default SiteMap;
