648e9face9a0d90bf82389463d43a38e6017ac55
francestu96 <francestu96@gmail.com>
2023-10-02 20:54:19 +0200

    Ads script added

--- src/templates/Default.tsx
+++ src/templates/Default.tsx
@@ -10,6 +10,7 @@ const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, page
       <title>{`${pageName}`}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" href="/favicon.ico"></link>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7675163173558825" crossOrigin="anonymous"></script>
     </Head>
     <Header />
     {children}
