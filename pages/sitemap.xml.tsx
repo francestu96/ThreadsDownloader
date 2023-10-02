db1d122429db7881429fb20476a9e1a098d71c61
francestu96 <francestu96@gmail.com>
2023-10-02 21:18:16 +0200

    sitemap update

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,4 +1,4 @@
-const URL = "https://threadsbro.studio";
+const URL = "https://threadsbro.link";
 
 function generateSiteMap(locales: string[]) {
     // return `<?xml version="1.0" encoding="UTF-8"?></xml>`;
--- src/templates/Default.tsx
+++ src/templates/Default.tsx
@@ -1,8 +1,8 @@
 import { FC, ReactNode } from 'react';
-import { Container } from '@chakra-ui/react';
 import Head from 'next/head';
 import Header from 'elements/Header';
 import Footer from 'elements/Footer';
+import Script from 'next/script';
 
 const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, pageName }) => (
   <>
@@ -10,8 +10,8 @@ const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, page
       <title>{`${pageName}`}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" href="/favicon.ico"></link>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7675163173558825" crossOrigin="anonymous"></script>
     </Head>
+    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7675163173558825" crossOrigin="anonymous"></Script>
     <Header />
     {children}
     <Footer />
