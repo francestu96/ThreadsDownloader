cbf678c23120a4dc9a3f2bba83f93f5f38b953e4
francestu96 <francestu96@gmail.com>
2023-10-03 00:03:23 +0200

    meta desc and keywords init

--- src/templates/Default.tsx
+++ src/templates/Default.tsx
@@ -3,19 +3,27 @@ import Head from 'next/head';
 import Header from 'elements/Header';
 import Footer from 'elements/Footer';
 import Script from 'next/script';
+import { useTranslation } from 'next-i18next';
 
-const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, pageName }) => (
-  <>
-    <Head>
-      <title>{`${pageName}`}</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" href="/favicon.ico"></link>
-    </Head>
-    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7675163173558825" crossOrigin="anonymous"></Script>
-    <Header />
-    {children}
-    <Footer />
-  </>
-);
+const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, pageName }) => {
+  const { t } = useTranslation();
+  
+  return (
+    <>
+      <Head>
+        <title>{`${pageName}`}</title>
+        <link rel="icon" href="/favicon.ico"/>
+        <meta charSet="utf-8"/>
+        <meta name="description" content={t("META.DESCRIPTION")}/>
+        <meta name="keywords" content={t("META.DESCRIPTION")}/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7675163173558825" crossOrigin="anonymous"></Script>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+};
 
 export default Default;
